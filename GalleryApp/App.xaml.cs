ab6658a7904e78aa7d4b86e4bf8c5a8eacd4f5d6
Edanriell <Edanriell@gmail.com>
2025-07-12 12:00:00 +0000

    initial commit

---
+++ GalleryApp/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace GalleryApp;
+
+public partial class App : Application
+{
+    public App() { InitializeComponent(); }
+
+    protected override Window CreateWindow(IActivationState? activationState) { return new Window(new AppShell()); }
+}
---
+++ GalleryApp/AppShell.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace GalleryApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell() { InitializeComponent(); }
+}
