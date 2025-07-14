b51e35ad139c750406c7c41f06939adafbecb5c4
Edanriell <Edanriell@gmail.com>
2025-07-14 12:00:00 +0000

    feature(importing-photos): implemented feature

---
+++ GalleryApp/Services/PhotoImporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using GalleryApp.Models;
+
+namespace GalleryApp.Services;
+
+internal partial class PhotoImporter : IPhotoImporter
+{
+    public partial Task<ObservableCollection<Photo>> Get(
+        int
+            start, int count, Quality quality);
+
+    public partial Task<ObservableCollection<Photo>>
+        Get(List<string> filenames, Quality quality);
+
+    private partial Task<string[]> Import();
+}
